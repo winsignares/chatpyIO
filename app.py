03c08138c440edd78ce7999908bcd7e032c1fada
william <elhacker01@nose.com>
2024-03-18 07:57:21 -0500

    elemento terminado

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-print("hola mundo")
+app = Flask(__name__)
+socketIO = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketIO.on('mensaje')
+def handle_mensaje(msg):
+    print(msg)
+    socketIO.emit('mensaje', msg)
+
+if __name__ == "__main__":
+    socketIO.run(app, debug=True)
+
+
